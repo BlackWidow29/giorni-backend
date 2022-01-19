274c391e144a71151891d65e36ea2735de1c0305
terrotar <pedro_guedes93@hotmail.com>
2022-01-18 22:04:53 -0300

    Adicionado alteraçoes com integracao de banco de dados; Inserido SQLite3 como exemplo e comentado a URI do postgreSQL

--- src/config.py
+++ src/config.py
@@ -1,7 +1,8 @@
 from distutils.debug import DEBUG
 
 
-SQLALCHEMY_DATABASE_URI = "postgresql://root:root@localhost/giorni"
+# SQLALCHEMY_DATABASE_URI = "postgresql://root:root@localhost/giorni"
+SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'secret'
 DEBUG = True
